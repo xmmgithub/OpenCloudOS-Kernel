877e48459e5925762adee8dd741c60dbb5b29c4e
Kan Liang <kan.liang@linux.intel.com>
2020-07-03 05:49:10 -0700

    perf/x86/intel/lbr: Add the function pointers for LBR save and restore
    The MSRs of Architectural LBR are different from previous model-specific
    LBR. Perf has to implement different functions to save and restore them.
    
    The function pointers for LBR save and restore are introduced. Perf
    should initialize the corresponding functions at boot time.
    
    The generic optimizations, e.g. avoiding restore LBR if no one else
    touched them, still apply for Architectural LBRs. The related codes are
    not moved to model-specific functions.
    
    Current model-specific LBR functions are set as default.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/1593780569-62993-5-git-send-email-kan.liang@linux.intel.com

--- arch/x86/events/intel/core.c
+++ arch/x86/events/intel/core.c
@@ -4258,6 +4258,8 @@ static __initconst const struct x86_pmu core_pmu = {
 
 	.lbr_reset		= intel_pmu_lbr_reset_64,
 	.lbr_read		= intel_pmu_lbr_read_64,
+	.lbr_save		= intel_pmu_lbr_save,
+	.lbr_restore		= intel_pmu_lbr_restore,
 };
 
 static __initconst const struct x86_pmu intel_pmu = {
@@ -4306,6 +4308,8 @@ static __initconst const struct x86_pmu intel_pmu = {
 
 	.lbr_reset		= intel_pmu_lbr_reset_64,
 	.lbr_read		= intel_pmu_lbr_read_64,
+	.lbr_save		= intel_pmu_lbr_save,
+	.lbr_restore		= intel_pmu_lbr_restore,
 };
 
 static __init void intel_clovertown_quirk(void)
