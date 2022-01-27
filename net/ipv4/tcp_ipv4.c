c870f28e94c9c7b7dd532e9c963ee65fd1c9e8ea
Menglong Dong <imagedong@tencent.com>
2022-01-27 17:13:01 +0800

    net: socket: rename SKB_DROP_REASON_SOCKET_FILTER
    Rename SKB_DROP_REASON_SOCKET_FILTER, which is used
    as the reason of skb drop out of socket filter before
    it's part of a released kernel. It will be used for
    more protocols than just TCP in future series.
    
    Signed-off-by: Menglong Dong <imagedong@tencent.com>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Link: https://lore.kernel.org/all/20220127091308.91401-2-imagedong@tencent.com/
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1951,7 +1951,7 @@ process:
 	nf_reset_ct(skb);
 
 	if (tcp_filter(sk, skb)) {
-		drop_reason = SKB_DROP_REASON_TCP_FILTER;
+		drop_reason = SKB_DROP_REASON_SOCKET_FILTER;
 		__NET_INC_DROPSTATS(net, LINUX_MIB_TCPFILTERDROP);
 		goto discard_and_relse;
 	}
